58cd038ff996cf15e67219b1a09e58a8699790ee
Rameel Sethi <rameel@qubit.com>
2017-04-09 02:11:40 -0600

    Rewrite Stanford script

--- stanford.py
+++ stanford.py
@@ -1,58 +1,46 @@
-from __future__ import division
 from argparse import ArgumentParser
-import json
-from subprocess import Popen, PIPE
-import sys
+from pycorenlp import StanfordCoreNLP
 
 
 def get_args():
-	parser = ArgumentParser(description='Stanford sentiment analysis wrapper')
-	parser.add_argument(
-		'-f', '--inputFile',
-		help='Input file',
-		required=True
-	)
-	return parser.parse_args()
+    parser = ArgumentParser(description='Stanford sentiment analysis wrapper')
+    parser.add_argument(
+        '-f', '--inputFile',
+        help='Input file',
+        required=True
+    )
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-	args = get_args()
-	inputFile = args.inputFile
-
-	count = 0
-	correct = 0
-
-	scores = {
-	    'Very negative': 1,
-	    'Negative': 2,
-	    'Neutral': 3,
-	    'Positive': 4,
-	    'Very positive': 5
-	}
-
-	with open(inputFile, 'r') as f:
-		for blob in f:
-			if blob:
-				review = json.loads(blob)
-				text = review['reviewText']
-				sentences = text.split('.')
-				expectedScore = review['overall']
-				print text, expectedScore
-				sentenceScores = []
-				p = Popen([
-					'java',
-					'-cp',
-					'stanford-corenlp-full-2016-10-31/*',
-					'-mx5g',
-					'edu.stanford.nlp.sentiment.SentimentPipeline',
-					'-stdin'
-				], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-				sentenceScores, err = p.communicate('\n'.join(sentences) + '\n')
-				sentenceScores = sentenceScores.split('\n')
-				sentenceScores = [scores[score.strip()] for score in sentenceScores if score]
-				averageScore = sum(sentenceScores) / len(sentenceScores)
-				print averageScore
-				if abs(averageScore - expectedScore) < 2:
-					correct += 1
-				count += 1
-		print correct / count
+    args = get_args()
+    inputFile = args.inputFile
+    nlp = StanfordCoreNLP('http://localhost:9000')
+    count = 0
+    correct = 0
+    with open(inputFile, 'r') as f:
+        for review in f:
+            print(review)
+            res = nlp.annotate(
+                review,
+                properties={
+                    'annotators': 'sentiment',
+                    'outputFormat': 'json',
+                    'timeout': 100000,
+                }
+            )
+            result = res['sentences']
+            numSentences = len(result)
+            if numSentences:
+                totalScore = sum([int(s['sentimentValue']) for s in result])
+                averageScore = totalScore / numSentences
+                print(averageScore)
+                if 'neg' in inputFile:
+                    if averageScore < 1.5:
+                        correct += 1
+                elif 'pos' in inputFile:
+                    if averageScore > 2.5:
+                        correct += 1
+                count += 1
+        accuracy = correct / count
+        print('Accuracy: {:.2f}'.format(correct / count))
